2a1384c938343f79a3006db902a6a8d1b7413118
msKoo <48276595+msKoo@users.noreply.github.com>
2021-07-30 15:59:59 +0900

    RTT translation first upload

---
+++ refining_Data/RTTtranslation.py
@@ -0,0 +1,75 @@
+import requests
+import pandas as pd
+
+
+def get_excel(path):
+    df_ = pd.read_excel(path)
+    data = pd.DataFrame()
+    querys = df_['질문 요약'].values.tolist()
+    answers = df_['답변 요약'].values.tolist()
+    Index = []
+    result_Q = []
+    result_A = []
+    for i in range(600, 700):
+        if (len(querys[i]) < 30) and (("누락" not in querys[i]) and ("누락" not in answers[i])):
+            # print(querys[i])
+            Index.append(i)
+            result_Q.append((querys[i]))
+            result_A.append(answers[i])
+    return Index, result_Q, df_
+
+
+def get_translate(text):
+    client_id = "SpPXEctX91xMitWem7Jv" # <-- client_id 기입
+    client_secret = "RHYrtKj49l" # <-- client_secret 기입
+
+    data = {'text' : text,
+            'source' : 'ko',
+            'target': 'ja'}
+
+
+
+    url = "https://openapi.naver.com/v1/papago/n2mt"
+
+    header = {"X-Naver-Client-Id":client_id,
+              "X-Naver-Client-Secret":client_secret}
+
+    response = requests.post(url, headers=header, data=data)
+    rescode = response.status_code
+
+    if(rescode==200):
+        send_data = response.json()
+        trans_data = (send_data['message']['result']['translatedText'])
+        # return trans_data
+    else:
+        print("Error Code:" , rescode)
+
+    data_reuslt = {'text': trans_data,
+                   'source': 'ja',
+                   'target': 'ko'}
+    response = requests.post(url, headers=header, data=data_reuslt)
+    rescode = response.status_code
+
+    if (rescode == 200):
+        send_data = response.json()
+        trans_data_ = (send_data['message']['result']['translatedText'])
+        return trans_data_
+    else:
+        print("Error Code:", rescode)
+
+Index, Question, Df = get_excel('src/요약본_4000개_원본_파파고.xlsx')
+
+# result_Df  = pd.Series(index=['질문', '질문 요약', '답변', '답변 요약'])
+result_Df = pd.DataFrame()
+
+trans = []
+for i in range (0, len(Index)):
+    trans.append(get_translate(Question[i]))
+    #trans.append(i)
+    print(trans[i])
+
+for i in range (0, len(Index)):
+    result_Df = result_Df.append(Df.loc[Index[i], ['질문', '질문 요약', '답변', '답변 요약']])
+
+result_Df['질문 요약'] = trans
+result_Df.to_excel('src/translated3.xlsx')
